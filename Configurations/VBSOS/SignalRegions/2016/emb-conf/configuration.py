c6e9689d60faaa0ad519988d7f64f991b318e3d7
Bianca Pinolini <b.pinolini@tim.it>
2020-12-07 10:28:59 +0100

    embedded configuration for 2016

---
+++ Configurations/VBSOS/SignalRegions/2016/emb-conf/configuration.py
@@ -0,0 +1,38 @@
+# example of configuration file
+
+tag = 'DNN_emb_comb_2016'
+outputDir = 'RootFiles'
+treeName = 'Events'
+
+date='201207'
+
+# luminosity to normalize to
+lumi = 35.867
+
+# file with TTree aliases
+aliasesFile = 'aliases.py'
+
+# file with list of variables
+variablesFile = 'variables.py'
+
+# file with list of cuts
+cutsFile = 'cuts.py'
+
+# file with list of samples
+samplesFile = 'samples.py'
+
+# file with list of plot
+plotFile = 'plot.py'
+
+# file with list of nuisances
+nuisancesFile = 'nuisances.py'
+
+# used by mkPlot to define output directory for plots
+# different from "outputDir" to do things more tidy
+outputDirPlots = 'output-plots'
+
+# used by mkDatacards to define output directory for datacards
+outputDirPlots = '/eos/home-b/bpinolin/www/VBSOS/2016/emb-conf/'+date
+
+# # structure file for datacard
+structureFile = 'structure.py'
