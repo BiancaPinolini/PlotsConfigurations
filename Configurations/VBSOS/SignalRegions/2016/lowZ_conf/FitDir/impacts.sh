b0725a5bfe2bd8a11bc6e954b8eed770cf076fc8
Bianca Pinolini <b.pinolini@tim.it>
2020-12-30 14:46:47 +0100

    impact plots

---
+++ Configurations/VBSOS/SignalRegions/2016/lowZ_conf/FitDir/impacts.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+## FIXME this is where the Combine framework is installed
+cd /afs/cern.ch/user/b/bpinolin/CMSSW_8_1_0/
+eval `scramv1 runtime -sh`
+cd -
+
+## FIXME this is your work directory
+date=201224/v3_plus
+workDir=/afs/cern.ch/user/b/bpinolin/CMSSW_10_6_4/src/PlotsConfigurations/Configurations/VBSOS/SignalRegions/2016/lowZ_conf/datacards
+var=DNNoutput_10
+
+datacardDir=${workDir}/${date}
+workspaceDir=${datacardDir}/workspace
+
+combineTool.py -M Impacts -d ${workspaceDir}/combine_${var}.root -m 125 -t -1 --setParameters r_vbs=1 --redefineSignalPOIs=r_vbs --X-rtd MINIMIZER_analytic --doInitialFit
+combineTool.py -M Impacts -d ${workspaceDir}/combine_${var}.root -m 125 -t -1 --setParameters r_vbs=1 --redefineSignalPOIs=r_vbs --X-rtd MINIMIZER_analytic --doFits
+combineTool.py -M Impacts -d ${workspaceDir}/combine_${var}.root -m 125 -t -1 --setParameters r_vbs=1 --redefineSignalPOIs=r_vbs --X-rtd MINIMIZER_analytic -o ${workspaceDir}/impacts_2016_${var}.json 
+
+cd $workspaceDir
+
+plotImpacts.py -i impacts_2016_${var}.json -o impacts_2016_${var}
